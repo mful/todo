3b75edc649b6bd0e63340f55488256a61a43f66f
matty <fulton.matthew@gmail.com>
2013-07-22 14:44:29 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+== README
+
+Goal: take the traditional Backbone Todo app and integrate it with rails and Marionette.js
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoBackbone::Application.config.secret_key_base = 'e898f2dfe7e1b26eecfe919236f3dabb88ac4b6c3a834f05bea2e1057fea97fe2b62ee75bdb676d6c0b983bcd47d61fac3ba4d11da827e000bd3f5c23053d982'
